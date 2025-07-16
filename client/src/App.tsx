399335f4c868b92cddeb0ceeb95cc1f5f9c8cf6a
Vijay Singh <vijayrana98120@gmail.com>
2025-07-16 03:49:39 -0700

    removed extra

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,8 @@
-import { Signup } from "./pages/signupPage"
-import { Login } from "./pages/loginPage"
-import { LandingPage } from "./pages/landingPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import EditorPage from "./pages/editorPage";
 import { Main } from "./pages/home";
 import NotesLandingPage from "./pages/landPage";
-import EditorPageTesting from "./pages/testing";
 import LoginPage from "./pages/logPage";
 import SignUpPage from "./pages/signPage";
 function App(){
@@ -19,7 +15,6 @@ function App(){
           <Route path='/signup' element={<SignUpPage />} />
           <Route path='/home' element = {<Main />} />
           <Route path='/editor' element={<EditorPage />} />
-          <Route path='/testing' element={<EditorPageTesting />} />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
